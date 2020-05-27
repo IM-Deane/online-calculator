fe9d6680764fbb66d45acde3dbc5a11cc2b0957c
Tristan <tristandeane93@gmail.com>
2020-05-27 10:01:00 -0600

    Added Version 2.1
    Contains an updated UI with a more complementary color scheme. Also streamlined calcCode.js by removing the memory data structure.
    The program will now get and set values directly to the UI screen instead of storing them to an array. This change allowed me to
    remove a function and several lines of now obsolete code.

--- calcCode.js
+++ calcCode.js
@@ -1,12 +1,13 @@
 const buttons = document.querySelectorAll('button');
-let memory = []; // Store input
+let screen = document.getElementById('screen');
+screen.textContent = 0; // Set initial value
 
-// Display the button once clicked
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
         console.log(e.target.id)
+        
         // If decimal is already present don't add 
-        if (memory.indexOf('.') != '-1' && e.target.name === '.') {
+        if (screen.textContent.indexOf('.') != '-1' && e.target.name === '.') {
             return;
         }
         // Skip displaying back and clear
@@ -15,24 +16,20 @@ buttons.forEach(button => {
             return;
         }
         // Check for first zero
-        if (document.getElementById('screen').innerHTML[0] == '0') {
-            document.getElementById('screen').innerHTML = '';
+        if (screen.textContent[0] == '0') {
+            screen.textContent = '';
         }
-        
         // Add input to memory and display
-        memory.push(e.target.name);
-        display(e.target.name);
+        //memory.push(e.target.name);
+        screen.textContent += e.target.name;
     });
 });
 
 // When equals button is pressed compute available memory
 document.getElementById('equals-btn').addEventListener('click', () => {
-    let result = compute(memory);
+    let result = evalute();
     // Clear screen and display answer
-    document.getElementById('screen').innerHTML = '';
-    display(result);
-    // Clear memory and then save answer
-    return memory = [result];
+    screen.textContent = result;
 });
 
 // Remove last item entered 
@@ -82,11 +79,6 @@ function operate(operator, a, b) {
 
 /* SECTION: HELPER FUNCTIONS */
 
-// Display input/output to working screen
-function display(item) {
-    document.getElementById('screen').innerHTML += item;
-}
-
 // Reset the window
 function clear() {
     window.location.reload();
@@ -94,10 +86,8 @@ function clear() {
 
 // Reverse last input by removing from screen and memory 
 function back() {
-    let str = document.getElementById('screen').innerHTML;
-    str = str.substring(0, str.length - 1);
-    document.getElementById('screen').innerHTML = str;
-    memory.pop();
+    screen.textContent = screen.textContent.slice(0, -1);
+    
 }
 
 // Check if decimal is present in memory
@@ -113,29 +103,25 @@ function checkDecimal(button) {
 }
 
 // Evaluate the memory and compute the present operations
-function compute(memory) {
+function evalute() {
     let pattern = new RegExp('[^0-9]', 'g'); // Search for non-digits
-    // Convert to string
-    let str = memory.join('');
-    console.log(memory)
-    let opIndex = str.search(pattern);
+    let opIndex = screen.textContent.search(pattern);
     // No operator found; answer = number entered
     if (opIndex == -1) {
-        return parseInt(str);
+        return parseInt(screen.textContent);
     }
-    return computeBedmas(memory);
+    return computeBedmas();
 }
 
 // Check for mulitple operators and evalute according to BEDMAS/PEDMAS
-function computeBedmas(memory) {
+function computeBedmas() {
     let result;
     // Capture operators and convert numbers to float if decimal is present
-    memory = document.getElementById('screen').innerHTML.split(/([*/+-])/g);
+   let memory = screen.textContent.split(/([*/+-])/g);
 
     // Check for the number of math operators present in array
     let countMultDiv = memory.filter(x => (x === '*' || x === '/')).length; // Div/mult
     let countAddSub = memory.filter(x => (x === '+' || x === '-')).length; // Add/sub
-
 
     // Use order of operations to compute the data stored in memory
     for (let i = 0; i < countMultDiv; i++){
